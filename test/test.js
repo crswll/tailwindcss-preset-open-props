62b17501b9d02acb5ccab169d232efd535bb84cd
Bill Criswell <bill.criswell@gmail.com>
2021-12-26 14:28:56 -0500

    test: update test flow (#3)

--- test/test.js
+++ test/test.js
@@ -1,22 +1,35 @@
-const { getMetaLookup } = require('../src/utils')
-const preset = require('../src')
+const { getMetaLookup } = require("../src/utils")
+const preset = require("../src")
 
-test('getMetaLookup', () => {
+test("getMetaLookup", () => {
   const lookup = getMetaLookup([
-    [ 'test', s => s === 'foo', s => s.toUpperCase() + 'MEOW' ],
+    ["test", (s) => s === "foo", (s) => s.toUpperCase() + "MEOW"],
   ])
 
-  const [ key, prefix, nameTransformer ] = lookup('foo', 123)
+  const [key, prefix, nameTransformer] = lookup("foo", 123)
 
-  expect(key).toBe('test')
-  expect(nameTransformer('foo')).toBe('FOOMEOW')
-  expect(prefix('foo')).toBe(true)
+  expect(key).toBe("test")
+  expect(nameTransformer("foo")).toBe("FOOMEOW")
+  expect(prefix("foo")).toBe(true)
 })
 
-describe('preset', () => {
-  test('theme should have proper keys', () => {
-    const keys = [ 'aspectRatio', 'backgroundImage', 'borderRadius', 'borderWidth', 'colors', 'fontSize', 'fontWeight', 'letterSpacing', 'lineHeight', 'spacing', 'zIndex', 'fontFamily' ]
-    const keyCheck = keys.every(key => !!preset.theme[key])
+describe("preset", () => {
+  test("theme should have proper keys", () => {
+    const keys = [
+      "aspectRatio",
+      "backgroundImage",
+      "borderRadius",
+      "borderWidth",
+      "colors",
+      "fontSize",
+      "fontWeight",
+      "letterSpacing",
+      "lineHeight",
+      "spacing",
+      "zIndex",
+      "fontFamily",
+    ]
+    const keyCheck = keys.every((key) => !!preset.theme[key])
 
     expect(keyCheck).toBe(true)
   })
