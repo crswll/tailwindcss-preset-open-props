e55b99436e0908080876a4fdd9b2d5e9db574dce
Bill Criswell <bill.criswell@gmail.com>
2021-12-26 19:58:04 -0500

    bundle properly maybe (#5)
    * bundle properly maybe
    * bump version

--- build.js
+++ build.js
@@ -1,12 +1,22 @@
 const esbuild = require('esbuild')
 
+let makeAllPackagesExternalPlugin = {
+  name: 'make-all-packages-external',
+  setup(build) {
+    // Must not start with "/" or "./" or "../"
+    let filter = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/
+    build.onResolve({ filter }, args => ({ path: args.path, external: true }))
+  },
+}
+
 const makeConfig = ({ format }) => ({
   entryPoints: ['src/index.js'],
   outfile: `dist/index.${format}.js`,
   format: format,
-  bundle: false,
+  bundle: true,
   platform: 'node',
+  plugins: [makeAllPackagesExternalPlugin]
 })
 
-esbuild.buildSync(makeConfig({ format: 'cjs' }))
-esbuild.buildSync(makeConfig({ format: 'esm' }))
+esbuild.build(makeConfig({ format: 'cjs' }))
+esbuild.build(makeConfig({ format: 'esm' }))
